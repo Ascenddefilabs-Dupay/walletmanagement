4f9ce2f542b3b4412acfc4ec19d9d5825e2f1401
PavanMallepoola <pavan.kumar@ascenddefi.com>
2024-08-28 11:20:44 +0530

    I have changed api end point's in backend for the walletmanagement

--- backend-wallet-management/walletmanagement/urls.py
+++ backend-wallet-management/walletmanagement/urls.py
@@ -1,24 +1,9 @@
-# # walletmanagement/urls.py
-# from django.urls import path
-# from .views import save_password  # Correct import
-# from .views import recovery_phrase,check_recovery_phrase
-# from django.urls import path
-# from .views import create_or_update_wallet
-
-# urlpatterns = [
-#     path('api/password/', save_password, name='save_password'),
-#     path('api/recovery-phrase/', recovery_phrase, name='recovery-phrase'),
-#     path('api/check-recovery-phrase/', check_recovery_phrase, name='check_recovery_phrase'),
-#     path('api/create-or-update-wallet/', create_or_update_wallet, name='create_or_update_wallet'),
-# ]
-
-# urls.py
 from django.urls import path
 from .views import save_wallet_data,check_recovery_phrase, update_password, get_latest_wallet_id
 
 urlpatterns = [
-    path('api/save-wallet-data/', save_wallet_data, name='save_wallet_data'),
-    path('api/check-recovery-phrase/', check_recovery_phrase, name='check_recovery_phrase'),
-    path('api/update-password/', update_password, name='update_password'),
-    path('api/latest_wallet_id/', get_latest_wallet_id, name='get_latest_wallet_id'),
+    path('walletmanagementapi/save-wallet-data/', save_wallet_data, name='save_wallet_data'),
+    path('walletmanagementapi/check-recovery-phrase/', check_recovery_phrase, name='check_recovery_phrase'),
+    path('walletmanagementapi/update-password/', update_password, name='update_password'),
+    path('walletmanagementapi/latest_wallet_id/', get_latest_wallet_id, name='get_latest_wallet_id'),
 ]
